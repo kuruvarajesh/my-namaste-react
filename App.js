89177f9247edd4d3ec09ee9ea97148338877a073
kuruvarajesh <kuruvarajesh607@gmail.com>
2025-07-04 17:45:31 +0530

    changes

--- App.js
+++ App.js
@@ -1,23 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// const heading = React.createElement("h1", { id: "heading", erlmcec: "heading-text" }, "Hello Rajesh, Welcome to React")
-// const root = ReactDOM.createRoot(document.getElementById("root"))
-// root.render(heading)
-
 
 /*
-*  <div id="parent">
-*       <div>
-            <h1> rfwr </h1>
-        </div>
-    </div>
+* Header
+*   - Logo
+*   - Nav items
+* Body
+*   - Search
+*   - ReastaurentContainer
+*       - RestaurantCard
+* Footer
+*   - Copyright
+*   - Links
+*   - Address
+*   - Contact
+* 
 */
 
-const parent = React.createElement("div",
-    {
-       id:"parent"
-    },
-    React.createElement("h1",{id:"heading2"},"h1 tag i'erewm")
-)
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(parent)
+const Head2 = (props) => {
+    console.log(props)
+}
+
+const head3 = (props) => {
+    console.log(props)
+}
+
+const HeadingComponent = () =>{
+    return(
+        <>
+        <Head2  names="Rjaesh" age={22}/>
+        {head3(names="Rajesh", age=22)}
+        </>
+    )
+}
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<HeadingComponent />)
